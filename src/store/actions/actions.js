9705b3643cc5643dc8305c1fb18c9758bc8af23f
dylangavilan <dylan.gavilan32@gmail.com>
2021-12-21 19:23:26 -0300

    feat: se agregan dispatchs

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,16 +1,18 @@
 import axios from "axios";
-
+import { GET_CATEGORIES, GET_TOP10 } from "./types";
 export function getClients() {
-    return function (dispatch) {
-        return axios.get("/api/product").then((res) => {
-            const products = res.data;
-            dispatch({ type: "GET_ALL", payload: products });
-        });
+    return async function (dispatch) {
+        const response = await axios.get(
+            "https://private-bf0ebc-n5101.apiary-mock.com/api/ctas/top10"
+        );
+        dispatch({ type: GET_TOP10, payload: response.data });
     };
 }
 export function getCategories() {
     return async function (dispatch) {
-        const brands = await axios.get("/api/brand");
-        dispatch({ type: "GET_BRANDS", payload: brands.data });
+        const brands = await axios.get(
+            "https://private-bf0ebc-n5101.apiary-mock.com/api/spa/categories"
+        );
+        dispatch({ type: GET_CATEGORIES, payload: brands.data });
     };
 }
