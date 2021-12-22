b61b54be6e784aeab6b3e6512689ff1bf857486a
dylangavilan <dylan.gavilan32@gmail.com>
2021-12-22 12:19:01 -0300

    feat: Se agregan estilos

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,11 +1,42 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCategories } from "../store/actions/actions";
+import { Grid, makeStyles, Typography, Container } from "@material-ui/core";
+const useStyle = makeStyles({
+    count: {
+        margin: " 0.5rem",
+        fontFamily: "bold",
+        color: "#0e1111",
+        padding: "2px 15px ",
+        borderRadius: "0.4rem",
+        fontSize: "1rem",
+    },
+});
 export default function Categories() {
     const dispatch = useDispatch();
+    const categories = useSelector((state) => state.categoryReducer.categories);
+    const classes = useStyle();
     useEffect(() => {
         dispatch(getCategories());
     }, [dispatch]);
 
-    return <div>{}</div>;
+    return (
+        <Container>
+            <Grid spacing={3} container direction="row" justifyContent="center">
+                {categories?.map((e) => (
+                    <Grid item xs={4}>
+                        <Typography>
+                            <span
+                                className={classes.count}
+                                style={{ backgroundColor: `${e.color}` }}
+                            >
+                                {e.ctasCount}
+                            </span>
+                            <span>{e.name}</span>
+                        </Typography>
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
+    );
 }
