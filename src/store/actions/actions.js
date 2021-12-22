0a2ed7544b8d280e02924900f734b9a50d65f88d
dylangavilan <dylan.gavilan32@gmail.com>
2021-12-22 13:53:19 -0300

    se agregan variables para llamados a la api

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,19 +1,15 @@
 import axios from "axios";
+import { CLIENT_API, CATEGORIES_API } from "./config";
 import { GET_CATEGORIES, GET_TOP10 } from "./types";
 export function getClients() {
     return async function (dispatch) {
-        const response = await axios.get(
-            "https://private-bf0ebc-n5101.apiary-mock.com/api/ctas/top10"
-        );
+        const response = await axios.get(CLIENT_API);
         dispatch({ type: GET_TOP10, payload: response.data.data });
     };
 }
 export function getCategories() {
-    console.log("entre");
     return async function (dispatch) {
-        const response = await axios.get(
-            "https://private-bf0ebc-n5101.apiary-mock.com/api/spa/categories"
-        );
+        const response = await axios.get(CATEGORIES_API);
         dispatch({ type: GET_CATEGORIES, payload: response.data.data });
     };
 }
---
+++ src/store/actions/config.js
@@ -0,0 +1,4 @@
+export const CLIENT_API =
+    "https://private-bf0ebc-n5101.apiary-mock.com/api/ctas/top10";
+export const CATEGORIES_API =
+    "https://private-bf0ebc-n5101.apiary-mock.com/api/spa/categories";
