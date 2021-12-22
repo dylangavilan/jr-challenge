8b3cd053a8a70ea53985f0f3c90969b0c2a304f2
dylangavilan <dylan.gavilan32@gmail.com>
2021-12-22 12:58:10 -0300

    se añadieron los estilos a la lista de clientes

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,11 +1,54 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getClients } from "../store/actions/actions";
+import { getCategories, getClients } from "../store/actions/actions";
+import {
+    Avatar,
+    List,
+    ListItem,
+    ListItemText,
+    ListItemAvatar,
+} from "@material-ui/core";
+
 export default function Clients() {
     const dispatch = useDispatch();
+    const clients = useSelector((state) => state.clientReducer.clients);
+    const categories = useSelector((state) => state.categoryReducer.categories);
     useEffect(() => {
         dispatch(getClients());
+        dispatch(getCategories());
     }, [dispatch]);
 
-    return <div>{}</div>;
+    return (
+        <List>
+            {clients?.map((el) => {
+                return (
+                    <ListItem>
+                        <ListItemAvatar>
+                            <Avatar
+                                style={{
+                                    backgroundColor:
+                                        categories[el.categoryId].color,
+                                }}
+                            >
+                                {el.customerName[0]}
+                            </Avatar>
+                        </ListItemAvatar>
+
+                        <ListItemText
+                            primary={`${el.customerName}  ${el.customerLastName}`}
+                            secondary={
+                                <span
+                                    style={{
+                                        color: categories[el.categoryId].color,
+                                    }}
+                                >
+                                    {el.title}: {el.groupDescription}
+                                </span>
+                            }
+                        />
+                    </ListItem>
+                );
+            })}
+        </List>
+    );
 }
